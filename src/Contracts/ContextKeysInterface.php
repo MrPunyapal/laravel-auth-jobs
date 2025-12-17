0d8677dd7df9cb168bd5ba9f3082105f0a0304f3
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-17 23:22:50 +0530

    added interface for enum

---
+++ src/Contracts/ContextKeysInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MrPunyapal\LaravelAuthJobs\Contracts;
+
+interface ContextKeysInterface
+{
+    /**
+     * Get the context key for storing the authenticated user's ID.
+     */
+    public static function authIdKey(): string;
+
+    /**
+     * Get the context key for storing the authentication guard name.
+     */
+    public static function authGuardKey(): string;
+}
